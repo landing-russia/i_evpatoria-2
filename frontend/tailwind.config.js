9fb69ed90fdceb0210eddd72d2dc845a81f49872
landing-russia <landing-russia@yandex.ru>
2021-05-26 17:47:29 +0300

    frontend-3

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,24 +2,24 @@ const colors = require("tailwindcss/colors");
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  purge: [
-    // "../tailwind/templates/**/*.html",
-    "../templates/**/*.html",
-    "./src/**/*.js",
-  ],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
-      },
-      colors: {
-        amber: colors.amber,
-      },
+    purge: [
+        // "../tailwind/templates/**/*.html",
+        "../templates/**/*.html",
+        "./src/**/*.js",
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                amber: colors.amber,
+            },
+        },
     },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    variants: {
+        extend: {},
+    },
+    plugins: [require('@tailwindcss/forms'),],
 };
